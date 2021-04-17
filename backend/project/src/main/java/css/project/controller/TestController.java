915bd4a0b2112cdb238497a3528156e69f64ca8f
Asoltanei Cristian <cristian.asoltanei@fortech.ro>
2021-04-17 15:36:37 +0300

    fix

--- backend/project/src/main/java/css/project/controller/TestController.java
+++ backend/project/src/main/java/css/project/controller/TestController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-=
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
   
   private final TestService testService;
